a7561371125e99d3517d0d997f43204f48afd01a
thanhntse <nguyenthaithanh07@gmail.com>
2025-09-24 20:20:02 +0700

    refactor: remove api project

--- apps/api/src/openai/dto/message.dto.ts
+++
@@ -1,9 +0,0 @@
-import { ApiProperty } from '@nestjs/swagger';
-
-export class MessageDto {
-  @ApiProperty({
-    description: 'Message content',
-    example: 'Hello, how can you help me today?',
-  })
-  content: any;
-}
--- apps/api/src/skin-profile/dto/update-skin-question.dto.ts
+++
@@ -1,4 +0,0 @@
-import { PartialType } from '@nestjs/mapped-types';
-import { CreateSkinQuestionDto } from './create-skin-question.dto';
-
-export class UpdateSkinQuestionDto extends PartialType(CreateSkinQuestionDto) {}
