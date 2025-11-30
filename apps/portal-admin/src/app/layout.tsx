9e6a50659be04f817a041eca2c54283c174c62ba
thanhntse <nguyenthaithanh07@gmail.com>
2025-11-30 16:17:49 +0700

    feat: admin refactor authentication handling with hooks and cookie management

--- apps/portal-admin/src/app/layout.tsx
+++ apps/portal-admin/src/app/layout.tsx
@@ -12,6 +12,7 @@ import './globals.css';
 import './theme.css';
 import { NextIntlClientProvider } from 'next-intl';
 import { getLocale, getMessages } from 'next-intl/server';
+import AuthInitializer from '@/components/auth/auth-initializer';
 
 const META_THEME_COLORS = {
   light: '#ffffff',
@@ -78,6 +79,7 @@ export default async function RootLayout({
               enableColorScheme
             >
               <Providers activeThemeValue={activeThemeValue as string}>
+                <AuthInitializer />
                 <Toaster />
                 {children}
               </Providers>
